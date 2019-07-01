9c16c4a1ff8499c3a95c4651e5d6f96557b8c760
Angela <8798027+angelabauer@users.noreply.github.com>
2019-07-01 12:15:38 +0100

    Initial Commit

---
+++ Calculator Layout iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator Layout iOS13
+//
+//  Created by Angela Yu on 01/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
